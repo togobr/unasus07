1935267c7a44e38b58953c62650f8be34f441742
togobr <togorafael@gmail.com>
2016-01-22 16:52:09 -0200

    Adicionando estilo dropdown para recurso revelar

--- clients/unasus/padraoScroll/js/desktop/resources/revelar/player.js
+++ clients/unasus/padraoScroll/js/desktop/resources/revelar/player.js
@@ -1,5 +1,7 @@
 define([], function() {
 	var revelar = function() {
+		var me = this;
+		
 		this.init = function() {
 			/*Checar Função compile para observar que objetos são atrelados ao objeto principal
 			- this.template
@@ -12,30 +14,51 @@ define([], function() {
 			$.extend(true, this, new Player.Helpers.resourceExtend(this, arguments));
 
 			var recurso = this,
-				$contentCapa = recurso.$el.find('.contentCapa');
-				$content = recurso.$el.find('.tagP');
+				$contentCapa = recurso.$el.find('.contentCapa'),
+				$content = recurso.$el.find('.tagP'),
+				$dropDown = recurso.$el.find('.dropdown-toggle'),
+				$dropDownContent = recurso.$el.find('.dropdown-menu');
 
 				$content.hide();
+				$dropDown.hide();
+
 
 				if(recurso.$el.hasClass('click')){
 					recurso.$el.on({
 						click: function(e) {
-							$content.toggle();
-							$contentCapa.toggle();
+							recurso.changeContent();
+						}
+					});
+				}else if(recurso.$el.hasClass('hover')){
+					recurso.$el.on({
+						mouseover: function(e) {
+							recurso.changeContent();
+						},
+						mouseout: function(e) {
+							recurso.changeContent();
 						}
 					});
 				}else{
-					recurso.$el.on({
-						mouseover: function(e) {
-							$content.toggle();
-							$contentCapa.toggle();
-						},
-						mouseout: function(e) {
-							$content.toggle();
-							$contentCapa.toggle();
-						}
-					});
+					recurso.changeContent();
 				}
+		},
+
+		this.changeContent = function() {
+			$contentCapa = this.$el.find('.contentCapa'),
+			$content = this.$el.find('.tagP'),
+			$dropDown = this.$el.find('.dropdown-toggle'),
+			$dropDownContent = this.$el.find('.dropdown-menu');
+
+			$content.toggle();
+			$contentCapa.toggle();
+
+			if(this.$el.hasClass('dropdown')){
+				$dropDown.show();
+				$content.hide();
+				$contentCapa.hide();
+			}
+
+			
 		}
 
 	}
