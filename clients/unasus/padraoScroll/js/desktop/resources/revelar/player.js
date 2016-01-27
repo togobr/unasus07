d8dfee7bbca6eab4334311254c6cb6972f1c5420
togobr <togorafael@gmail.com>
2016-01-27 16:21:34 -0200

    Estilado revelar, modal, sanfona

--- clients/unasus/padraoScroll/js/desktop/resources/revelar/player.js
+++ clients/unasus/padraoScroll/js/desktop/resources/revelar/player.js
@@ -14,54 +14,37 @@ define([], function() {
 			$.extend(true, this, new Player.Helpers.resourceExtend(this, arguments));
 
 			var recurso = this,
-				$contentCapa = recurso.$el.find('.contentCapa'),
+				$dropDownType = recurso.$el.find('.dropDownType'),
+				$clickType = recurso.$el.find('.clickType'),
+				$hoverType = recurso.$el.find('.hoverType')
 				$content = recurso.$el.find('.tagP'),
+				$contentCapa = recurso.$el.find('.contentCapa'),
 				$dropDown = recurso.$el.find('.dropdown-toggle'),
 				$dropDownContent = recurso.$el.find('.dropdown-menu');
 
-				$content.hide();
-				$dropDown.hide()
+				if(recurso.$el.hasClass("dropdown")){
+					$dropDownType.show();
+				}else if(recurso.$el.hasClass("click")){
+					$clickType.show();
 
-
-				if(recurso.$el.hasClass('click')){
 					recurso.$el.on({
 						click: function(e) {
-							recurso.changeContent();
-						}
-					});
-				}else if(recurso.$el.hasClass('hover')){
-					recurso.$el.on({
-						mouseover: function(e) {
-							recurso.changeContent();
-						},
-						mouseout: function(e) {
-							recurso.changeContent();
+							$(this).toggleClass('clicked');
 						}
 					});
 				}else{
-					recurso.changeContent();
-					$dropDown.css('display', 'block')
+					$hoverType.show();
+
+					recurso.$el.on({
+						mouseover: function(e) {
+							$(this).addClass('hovered'); 
+						},
+						mouseout: function(e) {
+							$(this).removeClass('hovered');
+						}
+					});
 				}
-		},
-
-		this.changeContent = function() {
-			$contentCapa = this.$el.find('.contentCapa'),
-			$content = this.$el.find('.tagP'),
-			$dropDown = this.$el.find('.dropdown-toggle'),
-			$dropDownContent = this.$el.find('.dropdown-menu');
-
-			$content.toggle();
-			$contentCapa.toggle();
-
-			if(this.$el.hasClass('dropdown')){
-				$dropDown.show();
-				$content.hide();
-				$contentCapa.hide();
-			}
-
-			
 		}
-
 	}
 
 	return revelar;
