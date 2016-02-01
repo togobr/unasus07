980986e5104fb24992d159a73b625b4fb5124552
Pedro <pedrojmuller@gmail.com>
2016-02-01 15:03:57 -0200

    botao pular secao

---
+++ clients/unasus/padraoScroll/js/desktop/resources/botaoPular/player.js
@@ -0,0 +1,48 @@
+define([], function() {
+    var botaoPular = function(template, data) {
+        this.init = function() {
+            /*Checar Função compile para observar que objetos são atrelados ao objeto principal
+			- this.template
+			- this.data
+			- this.compiled
+			- this.el
+			- this.$el
+			*/
+            var recurso = this.el;
+
+
+
+
+            Player.Elements.$content.on({
+                contentReady: function(e) {
+
+            
+
+                    $('a[href*="#"]:not([href="#"])').click(function() {
+                        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                            var target = $(this.hash);
+                            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                            if (target.length) {
+                                $('html, body').animate({
+                                    scrollTop: target.offset().top
+                                }, 1000);
+                                return false;
+                            }
+                        }
+                    });
+                }
+
+
+            });
+
+
+
+
+
+
+        }
+
+    }
+
+    return botaoPular;
+});
