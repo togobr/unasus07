878da6ee9856efaa77f4467d491f92d41d67375e
togobr <togorafael@gmail.com>
2016-02-11 08:51:17 -0200

    Recuperando scormdata

--- clients/unasus/padraoScroll/js/desktop/resources/gavetaHorizontal/player.js
+++ clients/unasus/padraoScroll/js/desktop/resources/gavetaHorizontal/player.js
@@ -15,14 +15,14 @@ define(["jqueryUiTabs"], function() {
 			var resource = this;
 
 
-			resource.renderGavetaHorizontal();
+			
 				
 			Player.Elements.$content.on({
 				contentReady: function() {	
 					var $gaveta = resource.$el.find("#tabsHor"),
 						firstTabVisited = resource.$el.find('ul li:first-child'),
 						resource_id = resource.data.id,
-						suspend_data = Player.Scorm.getScormValue('cmi.suspend_data').gavetaHorizontal,
+						suspend_data = Player.Scorm.getScormValue('cmi.suspend_data').gavetaHorizontal || {},
 						resource_scorm = {},
 						new_suspend_data,
 						tab = {};
@@ -41,42 +41,51 @@ define(["jqueryUiTabs"], function() {
 
 						Player.Scorm.setScormValue('cmi.suspend_data', 'gavetaHorizontal', new_suspend_data);
 						firstTabVisited.addClass('visited');
+
+						resource.renderGavetaHorizontal(suspend_data);
 				}
 			});
 		};
 
-		this.renderGavetaHorizontal= function() {
+		this.renderGavetaHorizontal= function(suspend_data) {
 			var me = this,
 				gavetaHorizontal = me.$el,
 				tabs_lis = me.$el.find('li'),
 				resource_id = me.data.id,
-				// scorm = Player.Scorm.getScormValue('cmi.suspend_data').gavetaHorizontal || {},
-				// resource_scorm = scorm[resource_id] || {},
+				scorm = suspend_data,
+				resource_scorm = scorm[resource_id] || {},
 				new_suspend_data;
 
 				gavetaHorizontal
-				.off('tabsactivate.setVisited')
-				.on('tabsactivate.setVisited', function(event, ui) {
-					
-					var i = tabs_lis.index(ui.newTab),
-						suspend_data = Player.Scorm.getScormValue('cmi.suspend_data').gavetaHorizontal,
-						resource_scorm = {},
-						new_suspend_data,
-						tab = {};
+					.off('tabsactivate.setVisited')
+					.on('tabsactivate.setVisited', function(event, ui) {
+						
+						var i = tabs_lis.index(ui.newTab),
+							suspend_data = Player.Scorm.getScormValue('cmi.suspend_data').gavetaHorizontal,
+							resource_scorm = {},
+							new_suspend_data,
+							tab = {};
 
-						tab[i] = {
-							visited: true
-						};
+							tab[i] = {
+								visited: true
+							};
 
-						resource_scorm[resource_id] = {
-							active: i,
-							tabs: tab
-						};
+							resource_scorm[resource_id] = {
+								active: i,
+								tabs: tab
+							};
 
-						new_suspend_data = $.extend(true, {}, suspend_data, resource_scorm);
+							new_suspend_data = $.extend(true, {}, suspend_data, resource_scorm);
 
-						Player.Scorm.setScormValue('cmi.suspend_data', 'gavetaHorizontal', new_suspend_data);
-						tabs_lis.eq(i).addClass('visited');
+							Player.Scorm.setScormValue('cmi.suspend_data', 'gavetaHorizontal', new_suspend_data);
+							tabs_lis.eq(i).addClass('visited');
+					});
+
+					console.log('foo me:', me);
+
+				//append tab content
+				$.each(me.data.gavetasHorizontais, function(i, tab) {
+					console.log('foo i / tab:', i, tab);
 				});
 		}
 
