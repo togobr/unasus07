5b088e84bed37feed010b6e4f0d62b94dee8a231
togobr <togorafael@gmail.com>
2016-03-17 17:51:45 -0300

    Teste build

--- clients/unasus/padraoScroll/js/desktop/main.js
+++ clients/unasus/padraoScroll/js/desktop/main.js
@@ -122,7 +122,8 @@ require.config({
         addIndicators: '../../../../../vendor/components/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators',
         gsap: '../../../../../vendor/components/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap',
         TweenMax: '../../../../../vendor/components/scrollmagic/scrollmagic/uncompressed/plugins/TweenMax.min',
-        TimelineMax: '../../../../../vendor/components/scrollmagic/scrollmagic/uncompressed/plugins/TimelineMax.min'
+        TimelineMax: '../../../../../vendor/components/scrollmagic/scrollmagic/uncompressed/plugins/TimelineMax.min',
+        TweenLite: '../../../../../vendor/components/scrollmagic/scrollmagic/uncompressed/plugins/TweenLite.min'
 	},
 
 	config: {
